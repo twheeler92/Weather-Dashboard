3a7d5ed456736a70ff4911ee4a94eb37e56ee52b
enable broom <82785041+twheeler92@users.noreply.github.com>
2021-06-11 11:06:34 -0400

    API returns measuments in imperial

--- script.js
+++ script.js
@@ -15,14 +15,35 @@ $('#searchButton').click(function () {
     userInput = document.getElementById('userInput').value;
     console.log("User input: ", userInput);
 
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey;
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&appid=' + APIKey;
 
     fetch(queryUrl)
         .then(function (res) {
             if (res.ok) {
                 res.json().then(function (test) {
                     console.log(test);
+                    renderWeather(test);
                 })
             }
         })
+    
+        
 });
+
+function renderWeather(data) {
+    var html = ""
+    
+    city = data.name.toString()
+    temp = data.main.temp;
+    descrip = data.weather[0].main.toString();
+
+    console.log('City: ', city);
+    console.log('Temprature: ', temp);
+    console.log('Weather description: ', descrip);
+
+}
+
+function render5Day() {
+
+
+}
