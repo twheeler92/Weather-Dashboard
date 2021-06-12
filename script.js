e3f872b451f9cc729d9642f20b1950d30fc7cc7c
enable broom <82785041+twheeler92@users.noreply.github.com>
2021-06-12 09:33:58 -0400

    Added 5 day cast and uv index coloring

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ $('#searchButton').click(function () {
                                     console.log(info);
                                     console.log(queryUrl);
                                     renderWeather(info, userCity);
+                                    render5Day(info);
                     
                 })
             }
@@ -56,8 +57,19 @@ function renderWeather(data, city) {
     humid = data.current.humidity;
     icon = data.current.weather[0].icon.toString();
     uv = data.current.uvi;
+    var indexUv = "";
 
-    var myHtml = '<div class="current"><h1>' + city + ' | ' + convertDate(date) + '</h1><p>Temp: ' + temp + ' F</p><p>Wind: ' + wind + ' MPH</p><p>Humidity: ' + humid + ' %</p><p>UV Index: ' + uv + '</p></div>';
+    if (uv <=2 ) {
+        indexUv ="low"
+    }
+    if (uv <=7) {
+        indexUv ="med"
+    }
+    if (uv >= 8) {
+        indexUv ="high"
+    }
+
+    var currentHtml = '<div class="current"><h1>' + city + ' | ' + convertDate(date) + '</h1><p>Temp: ' + temp + ' F</p><p>Wind: ' + wind + ' MPH</p><p>Humidity: ' + humid + ' %</p><p class="uv-index-' + indexUv + '">UV Index: ' + uv + '</p></div>';
 
     console.log('City: ', city);
     console.log('Date: ', convertDate(date));
@@ -67,11 +79,33 @@ function renderWeather(data, city) {
     console.log('Icon string: ', icon);
     console.log('Wins speed: ', wind);
 
-    $('#currentWeather').html(myHtml);
+    $('#currentWeather').html(currentHtml);
 
 }
 
-function render5Day() {
+function render5Day(data) {
+
+    for(var i = 1; i <= 5; i++) {
+
+        
+
+        dateFD =data.daily[i].dt;
+        iconFD =data.daily[i].weather[0].icon.toString();
+        tempFD = data.daily[i].temp.max;
+        windFD = data.daily[i].wind_speed;
+        humidFD = data.daily[i].humidity;
+        
+        var fiveDayHtml = '<div  class="fiveDAY"><h3>' + convertDate(dateFD) + '</h3><p>ICON: ' + iconFD + '</p><p>Temp: ' + tempFD + ' F</p><p>Wind: ' + windFD + ' MPH</p><p>Humidity: ' + humidFD + ' %</p></div>';
+
+        console.log('test five day :', i);
+        console.log('date: ', convertDate(dateFD));
+        console.log('icon: ', iconFD);
+        console.log('temp', tempFD);
+        console.log('wind', windFD);
+        console.log('humidity', humidFD);
+
+        $('#fiveDay').append(fiveDayHtml);
+    }
 
 
 }
